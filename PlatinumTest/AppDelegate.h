aef825d1748d927a38725e798052d85d9ee412c3
Joe Swindler <jswindler@tradestation.com>
2015-06-14 22:51:34 -0700

    Initial commit after some github issues.

---
+++ PlatinumTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlatinumTest
+//
+//  Created by Joe on 6/14/15.
+//  Copyright (c) 2015 Joe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlatinumTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlatinumTest
+//
+//  Created by Joe on 6/14/15.
+//  Copyright (c) 2015 Joe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
